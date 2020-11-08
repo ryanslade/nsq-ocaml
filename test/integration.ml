3c4d7e36a4e4c23539c0d56cab2c6f30e241a36e
Ryan Slade <ryanslade@gmail.com>
2020-11-08 22:14:10 +0200

    Add a simple integration test
    Simply publishes a few message and checks that they are consumed. It
    expects nsqd to be runnning on the default port on localhost.

---
+++ test/integration.ml
@@ -0,0 +1,44 @@
+open Base
+open Lwt
+open Lwt.Syntax
+
+let test_publish_and_consume _ () =
+  let open Nsq in
+  let address = Address.Host "localhost" in
+  let topic = Topic.Topic "Integration" in
+  let channel = Channel.Channel "integration" in
+  let payload = Bytes.of_string "message" in
+  let mvar = Lwt_mvar.create_empty () in
+  (* Handler returns payload to mvar *)
+  let handler data =
+    async (fun () -> Lwt_mvar.put mvar data);
+    return `Ok
+  in
+  let c = Consumer.create [ address ] topic channel handler in
+  (* Start consumer in background *)
+  async (fun () -> Consumer.run c);
+  let p = Producer.create address |> Result.ok_or_failwith in
+  (* Send single payload *)
+  let* res = Producer.publish p topic payload in
+  let () = Result.ok_or_failwith res in
+  let* res = Producer.publish_multi p topic [ payload; payload ] in
+  let () = Result.ok_or_failwith res in
+  (* At this point we should have payload queus up three times *)
+  let* pub1 = Lwt_mvar.take mvar in
+  let* multi1 = Lwt_mvar.take mvar in
+  let* multi2 = Lwt_mvar.take mvar in
+  Alcotest.(check bytes) "same payload" payload pub1;
+  Alcotest.(check bytes) "same payload" payload multi1;
+  Alcotest.(check bytes) "same payload" payload multi2;
+  return_unit
+
+let () =
+  Lwt_main.run
+  @@ Alcotest_lwt.run "integration"
+       [
+         ( "all",
+           [
+             Alcotest_lwt.test_case "publish and consume" `Quick
+               test_publish_and_consume;
+           ] );
+       ]
